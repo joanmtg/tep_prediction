baeac5452e3c7cae0346b26a4ac823e0c3a2ac49
Joan M. Tovar <joan.tovar@correounivalle.edu.co>
2020-04-14 18:14:49 -0500

    Patient data saved as CSV

--- tep_web/tep/views.py
+++ tep_web/tep/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .forms import PacienteForm, DiagnosticoForm
 from .models import Paciente
-#import csv
+import csv
+
+
+csv_columns = ['genero', 'edad','bebedor','fumador','otra_enfermedad',
+    'procedimiento_15dias','inmovilidad_inferior','viaje_prolongado','antecedentes_tep',
+    'malignidad','disnea','dolor_toracico','tos','hemoptisis','disautonomicos','edema_inferior',
+    'frec_respiratoria','so2','frec_cardiaca','pr_sistolica','pr_diastolica','fiebre','crepitos',
+    'sibilancias','soplos','wbc','hb','plt','derrame']    
 
 def registro_paciente(request):    
     if request.method == "POST":
@@ -16,11 +23,25 @@ def registro_paciente(request):
 
 def datos_medicos(request): 
     if request.method == "POST":
-        form = DiagnosticoForm(request.POST)
-        print(form)
+        form = DiagnosticoForm(request.POST)        
         if form.is_valid():
+            csv_file = 'tep/CSV/input.csv'
             print('VALID')
-            form.save()             
+            form.save()  
+
+            csv_data = [form.cleaned_data]
+            del csv_data[0]['paciente']
+            print(csv_data)
+            try:
+                with open(csv_file, 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                    writer.writeheader()
+                    for data in csv_data:
+                        print(data)
+                        writer.writerow(data)
+            except IOError as e:
+                print("I/O error", e)
+
     form = DiagnosticoForm()
     return render(request, 'tep/registro_diagnostico.html', {'form':form})
 
