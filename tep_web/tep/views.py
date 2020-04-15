499f82091efb9556d5689b21fd6e92c67d73e080
Joan M. Tovar <joan.tovar@correounivalle.edu.co>
2020-04-14 21:40:27 -0500

    Prediccion NN agregada

--- tep_web/tep/views.py
+++ tep_web/tep/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from .forms import PacienteForm, DiagnosticoForm
 from .models import Paciente
 import csv
+import rpy2.robjects as robjects
+from rpy2.robjects import r
 
 
 csv_columns = ['genero', 'edad','bebedor','fumador','otra_enfermedad',
@@ -25,22 +27,31 @@ def datos_medicos(request):
     if request.method == "POST":
         form = DiagnosticoForm(request.POST)        
         if form.is_valid():
-            csv_file = 'tep/CSV/input.csv'
-            print('VALID')
-            form.save()  
+            form.save()
+            csv_file = 'tep_web/tep/CSV/input.csv'            
 
-            csv_data = [form.cleaned_data]
-            del csv_data[0]['paciente']
-            print(csv_data)
+            patient_dict = [form.cleaned_data]
+            del patient_dict[0]['paciente']            
+
+            for key in patient_dict[0]:
+                attribute = patient_dict[0][key]
+                if isinstance(attribute,bool):
+                    if attribute:
+                        patient_dict[0][key] = 1
+                    else:
+                        patient_dict[0][key] = 0
+            
             try:
-                with open(csv_file, 'w') as csvfile:
+                with open(csv_file, 'w+') as csvfile:
                     writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
                     writer.writeheader()
-                    for data in csv_data:
-                        print(data)
+                    for data in patient_dict:
                         writer.writerow(data)
             except IOError as e:
                 print("I/O error", e)
+
+            result = r['source']('/home/joan/Desktop/Tesis/tep_prediction/tep_web/tep/CSV/tep_predict.R')
+            print("Predicción:",result[0][0][0])
 
     form = DiagnosticoForm()
     return render(request, 'tep/registro_diagnostico.html', {'form':form})
