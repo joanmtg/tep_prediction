ed22540d61e0ad00592e49a01973e5a195afe279
Joan M. Tovar <joan.tovar@correounivalle.edu.co>
2020-05-10 04:04:44 -0500

    se muestran botones de login

--- tep_web/tep_web/views.py
+++ tep_web/tep_web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 def login_view(request):
 
